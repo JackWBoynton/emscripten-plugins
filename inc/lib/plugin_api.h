fb720fcb6b5010545045091145908c6cc3954078
Jack Boynton <jboynton@beta.team>
2025-04-01 22:47:05 -0400

    fix dockerfile

--- inc/lib/plugin_api.h
+++ inc/lib/plugin_api.h
@@ -25,19 +25,19 @@ inline std::string getPluginArchitecture() {
     return "emscripten";
   #elif defined(__APPLE__)
     #if defined(__arm64__) || defined(__aarch64__)
-      return "macos_arm64";
+      return "macos-arm64";
     #else
-      return "macos_x86_64";
+      return "macos-x86_64";
     #endif
   #elif defined(_WIN32)
     return "windows";
   #elif defined(__linux__)
     #if defined(__x86_64__) || defined(_M_X64)
-      return "linux_x86_64";
+      return "linux-amd64";
     #elif defined(__i386__) || defined(_M_IX86)
-      return "linux_x86";
+      return "linux-i386";
     #elif defined(__aarch64__) || defined(_M_ARM64)
-      return "linux_arm64";
+      return "linux-arm64";
     #else
       #error "Unknown architecture"
     #endif
